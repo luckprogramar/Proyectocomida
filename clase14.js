83e704dfc1785d1ea48679cdd98b5382d9fa6ac6
luckprogramar <168375870+luckprogramar@users.noreply.github.com>
2024-05-14 00:51:14 -0300

    Add files via upload

---
+++ clase14.js
@@ -0,0 +1,12 @@
+console.log("je je")
+
+//let hora= parseInt(prompt("ingrese una hora"))
+//if (hora >=0 && hora< 24){ 
+//    document.write(" pusiste la hora")
+//}else{
+//    document.write(" NO, pero si")
+//}
+
+//for (let i= 1; i<=100; i++){
+  //  document.write(i, "<br>")
+//}
